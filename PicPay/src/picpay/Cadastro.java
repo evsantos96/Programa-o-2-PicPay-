3361aa738d5cd1c002e8d5774f5816c390579104
everton <evsantos96@gmail.com>
2018-10-01 04:53:25 -0300

    #Update Cadastro e Login, otimizando eventos e adicionando o layout final de ambos

--- PicPay/src/picpay/Cadastro.java
+++ PicPay/src/picpay/Cadastro.java
@@ -5,25 +5,195 @@
  */
 package picpay;
 
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 /**
  *
  * @author Everton
  */
 public class Cadastro extends JFrame {
+
+    JLabel jlCadastro0 = new JLabel("Cadastro");
+    JLabel jlCadastro = new JLabel("Digite seu Nome:");
+    JLabel jlCadastro2 = new JLabel("Digite seu Sobrenome:");
+    JLabel jlCadastro3 = new JLabel("Digite sua Senha:");
+    JLabel jlCadastro4 = new JLabel("Confirme sua Senha:");
+    JLabel jlCadastro5 = new JLabel("Digite seu CPF:");
+    JTextField jtCadastro = new JTextField("");
+    JTextField jtCadastro1 = new JTextField("");
+    JTextField jtCadastro2 = new JTextField("");
+    JPasswordField jpCadastro = new JPasswordField("");
+    JPasswordField jpCadastro2 = new JPasswordField("");
     JButton jbCadastro = new JButton("Criar Conta");
-    JLabel jlCadastro = new JLabel("Cadastrar");
+    JButton jbCadastro2 = new JButton("Fazer Login");
+
     public Cadastro() {
         super("Cadastro - PicPay");
-        setSize(450,450);
+        setSize(450, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         setLayout(null);
         setVisible(true);
+        Label();
+        Input();
+        Button();
     }
-    
+
+    public void Label() {
+        jlCadastro0.setFont(new Font("arial", 4, 35));
+        jlCadastro0.setBounds(140, 45, 160, 40);
+        jlCadastro0.setVisible(true);
+
+        add(jlCadastro0);
+        jlCadastro.setFont(new Font("arial", 1, 14));
+        jlCadastro.setBounds(55, 122, 190, 40);
+        jlCadastro.setVisible(true);
+        add(jlCadastro);
+        jlCadastro2.setFont(new Font("arial", 1, 14));
+        jlCadastro2.setBounds(10, 162, 190, 40);
+        jlCadastro2.setVisible(true);
+        add(jlCadastro2);
+        jlCadastro5.setFont(new Font("arial", 1, 14));
+        jlCadastro5.setBounds(71, 202, 190, 40);
+        jlCadastro5.setVisible(true);
+        add(jlCadastro5);
+        jlCadastro3.setFont(new Font("arial", 1, 14));
+        jlCadastro3.setBounds(55, 243, 190, 40);
+        jlCadastro3.setVisible(true);
+        add(jlCadastro3);
+        jlCadastro4.setFont(new Font("arial", 1, 14));
+        jlCadastro4.setBounds(30, 283, 190, 40);
+        jlCadastro4.setVisible(true);
+        add(jlCadastro4);
+    }
+
+    public void Input() {
+
+        jtCadastro.setBounds(210, 135, 180, 20);
+        jtCadastro.setVisible(true);
+        add(jtCadastro);
+
+        jtCadastro1.setBounds(210, 175, 180, 20);
+        jtCadastro1.setVisible(true);
+        add(jtCadastro1);
+
+        jtCadastro2.setBounds(210, 215, 180, 20);
+        jtCadastro2.setVisible(true);
+        add(jtCadastro2);
+
+        jpCadastro.setBounds(210, 255, 180, 20);
+        jpCadastro.setVisible(true);
+        add(jpCadastro);
+
+        jpCadastro2.setBounds(210, 295, 180, 20);
+        jpCadastro2.setVisible(true);
+        add(jpCadastro2);
+
+        TextFieldHandler handler = new TextFieldHandler();
+        jtCadastro.addFocusListener(handler);
+        jtCadastro1.addFocusListener(handler);
+        jtCadastro2.addFocusListener(handler);
+        jpCadastro.addFocusListener(handler);
+        jpCadastro2.addFocusListener(handler);
+
+    }
+
+    public void Button() {
+        jbCadastro.setBounds(163, 350, 120, 20);
+        jbCadastro.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jbCadastro.setBackground(Color.decode("#00c266"));
+        jbCadastro.setBorderPainted(false);;
+        jbCadastro.setVisible(true);
+        add(jbCadastro);
+        jbCadastro2.setToolTipText("Criar Conta");
+        jbCadastro2.setBounds(163, 395, 120, 20);
+        jbCadastro2.setBorderPainted(false);
+        jbCadastro2.setBackground(Color.decode("#eeeeee"));
+        jbCadastro2.setContentAreaFilled(false);
+        jbCadastro2.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        add(jbCadastro2);
+
+        ButtonHandler handler = new ButtonHandler();
+        jbCadastro.addActionListener(handler);
+        jbCadastro2.addActionListener(handler);
+    }
+
+    private class TextFieldHandler implements FocusListener {
+
+        @Override
+        public void focusGained(FocusEvent fe) {
+
+            if (fe.getSource() == jtCadastro) {
+                jtCadastro.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+            } else if (fe.getSource() == jtCadastro1) {
+                jtCadastro1.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+            } else if (fe.getSource() == jtCadastro2) {
+                jtCadastro2.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+            } else if (fe.getSource() == jpCadastro) {
+                jpCadastro.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+            } else {
+                jpCadastro2.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+            }
+
+        }
+
+        @Override
+        public void focusLost(FocusEvent fe) {
+            if (fe.getSource() == jtCadastro) {
+                jtCadastro.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+            } else if (fe.getSource() == jtCadastro1) {
+                jtCadastro1.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+            } else if (fe.getSource() == jtCadastro2) {
+                jtCadastro2.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+            } else if (fe.getSource() == jpCadastro) {
+                jpCadastro.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+            } else {
+                jpCadastro2.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+            }
+        }
+    }
+
+    private class ButtonHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if (ae.getSource() == jbCadastro2) {
+                int dialogButton = JOptionPane.YES_NO_OPTION;
+                int dialogResult = JOptionPane.showConfirmDialog(null, "Deseja Fazer Login?", "Voltar para Login", dialogButton);
+
+                if (dialogResult == 0) {
+                    dispose();
+                    System.gc();
+                    new Login();
+                    System.out.println("Yes option");
+                } else {
+                    System.out.println("No Option");
+                }
+            } else {
+                dispose();
+                new Login();
+                JOptionPane pane = new JOptionPane("Usuario Cadastrado \nJá pode fazer o seu Login");
+                JDialog dialog = pane.createDialog("Bem-Vindo");
+
+                dialog.setVisible(true);
+                dialog.setLocationRelativeTo(null);
+            }
+        }
+
+    }
 }
--- PicPay/src/picpay/Login.java
+++ PicPay/src/picpay/Login.java
@@ -27,7 +27,7 @@ import javax.swing.border.LineBorder;
  *
  * @author Everton
  */
-public class Login extends JFrame  {
+public class Login extends JFrame {
 
     JButton jbLogin = new JButton("Criar Conta");
     JButton jbLogin2 = new JButton("Login");
@@ -36,8 +36,12 @@ public class Login extends JFrame  {
     JLabel jlLogin3 = new JLabel("Senha:");
 
     JTextField jtLogin = new JTextField("PicPay2018@mail.com");
-    JPasswordField jpLogin = new JPasswordField("654321");
+    JPasswordField jpLogin = new JPasswordField("Picp");
 
+    /**
+     * Colocando atributos ao jframe e incicializando os outros metodos
+     * presentes no mesmo.
+     */
     public Login() {
 
         super("Bem-Vindo ao PicPay");
@@ -48,13 +52,13 @@ public class Login extends JFrame  {
         setLayout(null);
         setVisible(true);
 
-        Botoes();
+        Button();
         Label();
         Input();
     }
 
-    private void Botoes() {
-        
+    private void Button() {
+
         jbLogin.setToolTipText("Criar Conta");
         jbLogin.setBounds(163, 345, 120, 20);
         jbLogin.setBorderPainted(false);
@@ -62,138 +66,121 @@ public class Login extends JFrame  {
         jbLogin.setContentAreaFilled(false);
         jbLogin.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-       
         jbLogin2.setToolTipText("Login");
         jbLogin2.setBounds(163, 290, 120, 20);
         jbLogin2.setCursor(new Cursor(Cursor.HAND_CURSOR));
         jbLogin2.setBackground(Color.decode("#00c266"));
         jbLogin2.setBorderPainted(false);
         jbLogin2.setVisible(true);
+
         add(jbLogin);
         add(jbLogin2);
-        
-        
-        
+
         ButtonHandler Cadastro = new ButtonHandler();
         jbLogin.addActionListener(Cadastro);
-        
-        jButtonHandler Login = new jButtonHandler();
-        jbLogin2.addActionListener(Login);
-        
+        jbLogin2.addActionListener(Cadastro);
+
     }
 
     private void Label() {
         jlLogin.setFont(new Font("arial", 4, 35));
         jlLogin.setBounds(180, 45, 100, 40);
         jlLogin.setVisible(true);
-        
+
         jlLogin2.setFont(new Font("arial", 1, 14));
-        jlLogin2.setBounds(73, 155, 180, 20);
+        jlLogin2.setBounds(83, 155, 180, 20);
         jlLogin2.setVisible(true);
-        
+
         jlLogin3.setFont(new Font("arial", 1, 14));
-        jlLogin3.setBounds(70, 205, 180, 16);
+        jlLogin3.setBounds(75, 205, 180, 16);
         jlLogin3.setVisible(true);
-        
+
         add(jlLogin);
         add(jlLogin2);
         add(jlLogin3);
     }
 
     private void Input() {
-        jtLogin.setBounds(133, 155, 180, 20);
-        jtLogin.setFocusCycleRoot(false);
+        jtLogin.setBounds(153, 155, 180, 20);
         jtLogin.setVisible(true);
-        jpLogin.setBounds(133, 205, 180, 20);
+        jpLogin.setBounds(153, 205, 180, 20);
         jpLogin.setVisible(true);
 
         add(jtLogin);
         add(jpLogin);
-        TextFieldHandler email = new TextFieldHandler();
 
-        jtLogin.addFocusListener(email);
+        TextFieldHandler handler = new TextFieldHandler();
 
-        PasswordHandler senha = new PasswordHandler();
-        jpLogin.addFocusListener(senha);
+        jtLogin.addFocusListener(handler);
+        jpLogin.addFocusListener(handler);
 
     }
-private class ButtonHandler implements ActionListener{
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        int dialogButton = JOptionPane.YES_NO_OPTION;
-        int dialogResult = JOptionPane.showConfirmDialog(null, "Deseja Criar Conta?", "Criar Conta", dialogButton);
-          
-        if (dialogResult == 0) {
-            dispose();
-            new Cadastro();
-            
-            System.out.println("Yes option");
-        } else {
-            System.out.println("No Option");
-        }
-    }
-}
-    private class jButtonHandler implements ActionListener{
+
+    private class ButtonHandler implements ActionListener {
 
         @Override
-        public void actionPerformed(ActionEvent ae) {
-          dispose();
-          new Menu();
-         JOptionPane pane = new JOptionPane("Bem-VIndo Usuario");
-         JDialog dialog = pane.createDialog("Bem-Vindo");
-            dialog.setVisible(true);
-            dialog.setLocationRelativeTo(null);
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == jbLogin) {
+                int dialogButton = JOptionPane.YES_NO_OPTION;
+                int dialogResult = JOptionPane.showConfirmDialog(null, "Deseja Criar Conta?", "Criar Conta", dialogButton);
+
+                if (dialogResult == 0) {
+                    dispose();
+                    System.gc();
+                    new Cadastro();
+                    System.out.println("Yes option");
+                } else {
+                    System.out.println("No Option");
+                }
+            } else {
+                dispose();
+                
+                new Menu();
+                System.gc();
+                JOptionPane pane = new JOptionPane("Bem-VIndo Usuario");
+                JDialog dialog = pane.createDialog("Bem-Vindo");
+                dialog.setVisible(true);
+                dialog.setLocationRelativeTo(null);
+            }
         }
-    
-    
-    
-    } 
+    }
+
     private class TextFieldHandler implements FocusListener {
 
         @Override
         public void focusGained(FocusEvent e) {
-            if (jtLogin.getText().equals("PicPay2018@mail.com")) {
-                jtLogin.setText("");
+            if (e.getSource() == jtLogin) {
+                if (jtLogin.getText().equals("PicPay2018@mail.com")) {
+                    jtLogin.setText("");
 
+                }
+                jtLogin.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
+
+            } else {
+                if (jpLogin.getPassword().length == 4) {
+                    jpLogin.setText("");
+                }
+                jpLogin.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
             }
-            jtLogin.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
 
         }
 
         @Override
         public void focusLost(FocusEvent e) {
-            if (jtLogin.getText().equals("")) {
-                jtLogin.setText("PicPay2018@mail.com");
+            if (e.getSource() == jtLogin) {
+                if (jtLogin.getText().equals("")) {
+                    jtLogin.setText("PicPay2018@mail.com");
+                }
+                jtLogin.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
+
+            } else {
+                if (jpLogin.getPassword().length == 0) {
+                    jpLogin.setText("Picp");
+                }
+                jpLogin.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
             }
-            jtLogin.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
-
-        }
-    }
-
-    public class PasswordHandler implements FocusListener {
-
-        @Override
-        public void focusGained(FocusEvent fe) {
-            if (jpLogin.getPassword().length == 6) {
-                jpLogin.setText("");
-            }
-            jpLogin.setBorder(new LineBorder(Color.decode("#66afe9"), 1));
         }
 
-        @Override
-        public void focusLost(FocusEvent fe) {
-            if (jpLogin.getPassword().length == 0) {
-                jpLogin.setText("654321");
-            }
-            jpLogin.setBorder(new LineBorder(Color.decode("#a8a8a8"), 1));
-        }
     }
 
 }
-
-    
-
-/*
-  
- 
-*/
